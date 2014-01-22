ee592159fbbde0cddd17643ac66eb7b939ac2bf8
marc rosanes <mrosanes@cells.es>
2014-01-22 15:58:33 +0100

    add PyTango Proxy and set Position attribute
    Added PyTango Proxy and set Position attribute for it.
    We continue moving if the state is not ON. This implies that the
    state is most probably moving. When the state is ON we launch the
    following Position movement.

--- claess/clear/motors/testStopCode.py
+++ claess/clear/motors/testStopCode.py
@@ -1,8 +1,9 @@
 import unittest
 import random
+import PyTango
 
-#importat per poder utilitzar la macro mv i altres:
-#import sardana.macroserver.macros 
+#importat per poder utilitzar la macro mv i altres: (maybe)
+#import sardana.macroserver.macros (maybe)
 
 # SCRIPT USED TO SEE IF WE CAN REALIZE 50 MOVEMENTS WITHOUT SEEING AN ANORMAL
 # STOPCODE: FOR INSTANCE WE DO NOT WANT "SETTLING TIME ERROR"
@@ -21,6 +22,8 @@ import random
 # angles lower than 55degrees loses too much resolution.
 
 
+# The user will give as input the name of the motor to be tested.
+dummot = PyTango.DeviceProxy("motor/dummotctrl/7") 
 
 
 
@@ -33,24 +36,30 @@ text_file = open("Output.txt", "w")
 
 count = 0
 
-# 5*len(positions) = 5*8 = 48 movements
-for i in range(0, 6):
+print("Position at the very beginning of the script {0}:\n".format(dummot.Position))
 
-    positions = [50.0, 54.5, 57.777, 60.569, 64.125, 68.478, 71.333, 72.999]
-    print(positions)
+# 5*len(positions) = 5*8 = 48 movements
+for i in range(0, 1):
+#for i in range(0, 6):
+
+    #positions = [50.0, 54.5, 57.777, 60.569, 64.125, 68.478, 71.333, 72.999]
+    positions = [60.0, 58.5]
 
     for j in range(0, len(positions)): 
-
+        
         count = count+1
         pos = random.choice(positions)
         positions.remove(pos)
-        
-        #mv pseudomotortotest pos
+        dummot.Position = pos
+
+        motor_on = "ON"
+        while (motor_on not in str(dummot.State())): 
+            pass
         
         stopcode = "hiho"
 
         text_file.write("StopCode {0} at pos {1} is: {2}\n".format(count, pos, stopcode))
-    
+        print("Position after moving to the new position is {0}:\n".format(dummot.Position))
     
 text_file.close()
 
