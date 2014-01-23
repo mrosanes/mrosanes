2b6005092b8b1b0bdd91b0359ce3a1686060e264
marc rosanes <mrosanes@cells.es>
2014-01-23 17:20:08 +0100

    Change script in order to include the User inputs

--- claess/clear/motors/testStopCode.py
+++ claess/clear/motors/testStopCode.py
@@ -1,66 +1,99 @@
+# HOW TO CALL THE SCRIPT:
+# python testStopCode.py 58 62 63.36 --n=3 --c=True --mot=motorname
+
 import unittest
 import random
 import PyTango
+import argparse
 
-#importat per poder utilitzar la macro mv i altres: (maybe)
-#import sardana.macroserver.macros (maybe)
 
 # SCRIPT USED TO SEE IF WE CAN REALIZE 50 MOVEMENTS WITHOUT SEEING AN ANORMAL
-# STOPCODE: FOR INSTANCE WE DO NOT WANT "SETTLING TIME ERROR"
+# STOPCODE: FOR INSTANCE WE DO NOT WANT "SETTLING TIME ERROR"    
 
 
-# Basar-me amb els scripts motorcharacterization (the one from rhoms and the
-# one from mrosanes)
+parser = argparse.ArgumentParser(description='test a motor StopCode')
 
+parser.add_argument('pos', metavar='pos', type=float, nargs='+', 
+                   help='positions as list of floats')                   
+parser.add_argument('--mot', '--motor', type=str, default='motor/dummotctrl/7',
+                   help='pseudomotor or motor to be studied')
+parser.add_argument('--n','--ntimes', type=int, default=1,
+                   help='number of times that each position will be tested')
+parser.add_argument('--c','--clear', type=bool, default=False,
+                   help='Give True if you want to test CLEAR bragg pseudo')
 
-# Our limits for positions given by the Clear structure are: 
-# Lower limit: 43degrees 
-# Higher limit: 76degrees
-
-# Thus, to do safe movements we will move between 50degrees and 73degrees. 
-# Moreover, in a paper indicated by Iulian, it seems that 
-# angles lower than 55degrees loses too much resolution.
-
-
-# The user will give as input the name of the motor to be tested.
-dummot = PyTango.DeviceProxy("motor/dummotctrl/7") 
+args = parser.parse_args()
 
 
 
-# Preguntar com a input quin dels motors volem moure
-# pseudomotortotest = braggth ###########a reposar al codi
 
-#positions = [60, 65] ######### sol com a test del test #(basura)
+position_list = args.pos
 
-text_file = open("Output.txt", "w")
 
+########### Limits for CLEAR in bragg Pseudomotor ###########
+#Clear bragg pseudo must be limited (for the moment) to 50degrees and 73degrees.
+
+low_lim = 50
+high_lim = 73
+
+if (args.c == True):
+    for i in range (0, len(position_list)):
+        if (position_list[i]>= high_lim):
+            position_list[i] = high_lim
+        elif (position_list[i]<= low_lim):
+            position_list[i] = low_lim
+        else: 
+            position_list[i] = position_list[i]
+
+##############################################################
+
+
+
+print(position_list)
+
+motor = PyTango.DeviceProxy(args.mot)
+
+text_file = open("StopCodes.txt", "w")
 count = 0
 
-print("Position at the very beginning of the script {0}:\n".format(dummot.Position))
+print("\nPosition at the beginning of the script is {0}:\n"
+                                            .format(motor.Position))
 
-# 5*len(positions) = 5*8 = 48 movements
-for i in range(0, 1):
-#for i in range(0, 6):
 
-    #positions = [50.0, 54.5, 57.777, 60.569, 64.125, 68.478, 71.333, 72.999]
-    positions = [60.0, 58.5]
+for i in range(0, args.n):
+
+    positions = []
+
+    for i in range(0, len(position_list)):
+        positions.append(position_list[i])
+
+    print("\nPositions: {0}".format(positions))
 
     for j in range(0, len(positions)): 
         
         count = count+1
-        pos = random.choice(positions)
-        positions.remove(pos)
-        dummot.Position = pos
+        posit = random.choice(positions)
+        positions.remove(posit)
+        motor.Position = posit
 
         motor_on = "ON"
-        while (motor_on not in str(dummot.State())): 
+        while (motor_on not in str(motor.State())): 
             pass
-        
+ 
+        motor.read_attribute("Status")
+        #stopcode = motor.read_attribute("StatusStopCode").value
         stopcode = "hiho"
 
-        text_file.write("StopCode {0} at pos {1} is: {2}\n".format(count, pos, stopcode))
-        print("Position after moving to the new position is {0}:\n".format(dummot.Position))
+
+        text_file.write("StopCode {0} at position {1} is: {2}\n"
+                                        .format(count, posit, stopcode))
+
+        print("StopCode {0} at position {1} is: {2}"
+                                        .format(count, posit, stopcode))
     
+
+
 text_file.close()
+print('\n')
 
 
