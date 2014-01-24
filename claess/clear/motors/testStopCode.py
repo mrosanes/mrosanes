2f49f44f75c3713be3b1cfd5282d6a9756d61c50
marc rosanes <mrosanes@cells.es>
2014-01-24 13:05:33 +0100

    Further modifications in script testStopCode and file motors

--- claess/clear/motors/testStopCode.py
+++ claess/clear/motors/testStopCode.py
@@ -1,5 +1,11 @@
 # HOW TO CALL THE SCRIPT:
-# python testStopCode.py 58 62 63.36 --n=3 --c=True --mot=motorname
+# python testStopCode.py motors --clear=True --n=1
+# Where 'motors' is a file containing a list with pseudomotor, motors and 
+# positions.
+
+
+# SCRIPT USED TO SEE IF WE CAN REALIZE 50 MOVEMENTS WITHOUT SEEING AN ANORMAL
+# STOPCODE: FOR INSTANCE WE DO NOT WANT "SETTLING TIME ERROR"  
 
 import unittest
 import random
@@ -7,23 +13,17 @@ import PyTango
 import argparse
 
 
-# SCRIPT USED TO SEE IF WE CAN REALIZE 50 MOVEMENTS WITHOUT SEEING AN ANORMAL
-# STOPCODE: FOR INSTANCE WE DO NOT WANT "SETTLING TIME ERROR"    
-
 
 parser = argparse.ArgumentParser(description='test a motor StopCode')
 
 parser.add_argument('file', metavar='fname', type=str,
                    help='list with pseudomotor and physical motors composing' + 
 			            'it, to be tested: StopCode')   
-parser.add_argument('pos', metavar='pos', type=float, nargs='+', 
-                   help='positions as list of floats')                   
-parser.add_argument('--mot', '--motor', type=str, default='motor/dummotctrl/7',
-                   help='pseudomotor or motor to be studied')
 parser.add_argument('--n','--ntimes', type=int, default=1,
                    help='number of times that each position will be tested')
 parser.add_argument('--c','--clear', type=bool, default=False,
                    help='Give True if you want to test CLEAR bragg pseudo')
+
 
 args = parser.parse_args()
 
@@ -31,6 +31,7 @@ args = parser.parse_args()
 
 
 f = open(args.file, 'r')
+text_file = open("StopCodes.txt", "w")
 
 # The motor_list shall contain the pseudomotor as first motor in the list and 
 # all the physical motors listed after it.
@@ -42,10 +43,12 @@ pseudomotor_name = motors_list[0]
 physical_motors_names = motors_list[1:]
 
 print('\nPseudomotor is: {0}'.format(pseudomotor_name))
-print('Physical motors are: {0}\n'.format(physical_motors_names))
+text_file.write('\nPseudomotor is: {0}\n'.format(pseudomotor_name))
+
+print('Physical motors are: {0}'.format(physical_motors_names))
+text_file.write('Physical motors are: {0}\n'.format(physical_motors_names))
 
 pseudomotor = PyTango.DeviceProxy(pseudomotor_name)
-
 
 
 physical_motors = []
@@ -56,16 +59,15 @@ for i in range (0, len(physical_motors_names)):
 
 
 positions = f.readline()
-positions_str_list = positions.split()
-positions_list=[]
-for i in range (0, len(positions_str_list)):
-    positions_list.append(float(positions_str_list[i]))    
+position_str_list = positions.split()
+position_list=[]
+for i in range (0, len(position_str_list)):
+    position_list.append(float(position_str_list[i]))    
    
-print('Positions to be reach for pseudo are: {0}\n'.format(positions_list))
 
 
-"""
-########### Limits for CLEAR in bragg Pseudomotor ###########
+###########   Limits for CLEAR in bragg Pseudomotor   ###########
+########### Indicate --clear=True in the script call  ###########
 #Clear bragg pseudo must be limited (for the moment) to 50degrees and 73degrees.
 
 low_lim = 50
@@ -82,23 +84,17 @@ if (args.c == True):
 
 ##############################################################
 
+print('Positions to be reach for pseudo are: {0}\n'.format(position_list))
+text_file.write('Positions to be reach for pseudo are: {0}\n'.format(position_list))
 
 
-print(position_list)
 
-# PseudoMotor
-motor = PyTango.DeviceProxy(args.mot)
-
-motor1 = PyTango.DeviceProxy(args.mot)
-motor2 = PyTango.DeviceProxy(args.mot)
-
-
-text_file = open("StopCodes.txt", "w")
 count = 0
 
-print("\nPosition at the beginning of the script is {0}:\n"
-                                            .format(motor.Position))
-
+print('\nPosition of pseudo at the beginning of the script is {0}:\n'
+                                            .format(pseudomotor.Position))
+text_file.write('\nPosition of pseudo at the beginning of the script is {0}:\n'
+                                            .format(pseudomotor.Position))
 
 for i in range(0, args.n):
 
@@ -107,33 +103,42 @@ for i in range(0, args.n):
     for i in range(0, len(position_list)):
         positions.append(position_list[i])
 
+    text_file.write('---------\n')
+    text_file.write("\nPositions: {0}\n".format(positions))
     print("\nPositions: {0}".format(positions))
+    
 
     for j in range(0, len(positions)): 
-        
+                
         count = count+1
         posit = random.choice(positions)
         positions.remove(posit)
-        motor.Position = posit
+        pseudomotor.Position = posit
 
         motor_on = "ON"
-        while (motor_on not in str(motor.State())): 
+        while (motor_on not in str(pseudomotor.State())): 
             pass
  
-        motor.read_attribute("Status")
-        #stopcode = motor.read_attribute("StatusStopCode").value
-        stopcode = "hiho"
 
+        for u in range(0, len(physical_motors)):
+            physical_motors[u].read_attribute("Status")    
 
-        text_file.write("StopCode {0} at position {1} is: {2}\n"
-                                        .format(count, posit, stopcode))
+            try:            
+                stopcode = physical_motors[u].read_attribute("StatusStopCode").value
+            except:
+                stopcode = "StopCode does NOT exist for this motor"
+        
+            text_file.write('StopCode at pseudoposition {0} for physicalmotor {1} is: {2}\n'
+            .format(pseudomotor.Position, physical_motors[u].alias(), stopcode))
 
-        print("StopCode {0} at position {1} is: {2}"
-                                        .format(count, posit, stopcode))
+            print('StopCode at pseudoposition {0} for physicalmotor {1} is: {2}'
+                          .format(posit, physical_motors[u].alias(), stopcode))
     
+        text_file.write('\n')
 
 
+f.close()
 text_file.close()
 print('\n')
-"""
+
 
